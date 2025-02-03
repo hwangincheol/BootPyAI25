0549150450d5bcdd304630162291e37cf946e1dc
hwangincheol <159992861+hwangincheol@users.noreply.github.com>
2025-02-03 10:58:52 +0900

    Update HomeController.java

--- src/main/java/edu/mbcai/pybootai/controller/HomeController.java
+++ src/main/java/edu/mbcai/pybootai/controller/HomeController.java
@@ -11,14 +11,6 @@ public class HomeController {
         return "index"; // resources/templates/index.html을 찾아감
     } // http://localhost:80/  반응하는 컨트롤러 -> 테스트 완료
 
-    @GetMapping("/video")
-    public String video() {
-        return "video";
-    }
-
-    @GetMapping("/photo")
-    public String photo() {
-        return "photo";
-    }
+    
 
 }
